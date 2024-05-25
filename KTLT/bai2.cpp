d79cfe5b2bba1f9fa2b14ae56457c2adcaa37494
Keicaster <144100090+Keicaster@users.noreply.github.com>
2024-05-25 15:01:38 +0700

    Add files via upload

---
+++ KTLT/bai2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main ()
+{
+
+int x = 2 ;
+int y = 3 ;
+int tong, hieu, tich, thuong;
+tong = x + y;
+hieu = x - y;
+tich = x * y;
+thuong = x / y;
+cout<< "Tong la " << tong << "." << endl;
+cout<< "Hieu la " << hieu << "." << endl;
+cout<< "Tich la " << tich << "." << endl;
+cout<< "Thuong la " << thuong << "." << endl;
+return 0;
+}
---
+++ KTLT/tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Toi moi 10 tuoi" << endl;
+  cout << "Toi con qua tre de choi game";
+  return 0;
+}
